738db4fead49e0debc63db44ec37b16a2926042a
Daniel Hernandez Stalhuth <hsdaniele95@gmail.com>
2024-01-17 11:50:24 +0100

    .

--- backend/models/Appointment.js
+++ backend/models/Appointment.js
@@ -5,6 +5,7 @@ const appointmentSchema = new mongoose.Schema({
   email: { type: String, required: true },
   date: { type: Date, required: true },
   treatmentType: { type: String, required: true },
+  time: { type: String, required: true },
 });
 
 const Appointment = mongoose.model('Appointment', appointmentSchema);
